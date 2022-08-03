9c5e96655dd4c1e09b57fa08137c278264dc6c0a
oryo23-meet <109899438+oryo23-meet@users.noreply.github.com>
2022-08-03 10:31:38 +0300

    update

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -23,18 +23,21 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 #Code goes below here
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+	#return redirect(url_for("signin"))
+	#return render_template("signin.html")
 	error = ""
 	if request.method == 'POST':
 		email = request.form['email']
 		password = request.form['password']
 		try:
 			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-			return redirect(url_for('add_tweet'))
+			return redirect(url_for('home'))
 		except:
 			error = "Authentication failed"
 			return redirect(url_for("signin"))
 	else:
 		return render_template("signin.html")
+	 
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -46,8 +49,8 @@ def signup():
 		try:
 			login_session['user'] = auth.create_user_with_email_and_password(email, password)
 			user = {
-			"email": request.form['email'], "password" : request.form['password'], "full_name" : request.form['full_name'],
-			"user_name" : request.form['user_name'], "bio" : request.form['bio']
+			"email": request.form['email'], "password" : request.form['password'], "full_name" : request.form['fname'],
+			"user_name" : request.form['uname'], "bio" : request.form['bio']
 			}
 			db.child("users").child(login_session['user']["localId"]).set(user)
 			return redirect(url_for('signin'))
@@ -61,7 +64,7 @@ def signup():
 def add_tweet():
 	if request.method == 'POST':
 		tweet = {
-		"title" : request.form['title'], "bio" : request.form['bio'] 
+		"title" : request.form['title'], "bio" : request.form['bio'], "uid" : login_session['user']["localId"] 
 		}
 		db.child("articles").push(tweet)
 	return render_template("add_tweet.html")
@@ -69,10 +72,13 @@ def add_tweet():
 @app.route('/tweets', methods=['GET', 'POST'])
 def all_tweet():
 	tweets = db.child("articles").get().val()
+	users = db.child("users").get().val()
+	return render_template("tweets.html", tweets= tweets, users=users)
 
-	return render_template("tweets.html", tweets= tweets)
-
-
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	error = ""
+	return render_template("home.html")
 
 
 #Code goes above here
